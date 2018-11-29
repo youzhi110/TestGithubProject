31849c12c15a807270c27198502d2d9662e5ff32
youzhi110 <958573875@qq.com>
2018-11-29 11:21:17 +0800

    haha

---
+++ TestGithubProject/src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptTest
+ * discription:
+ * author:mx
+ * createTime:2018/11/29 9:07
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("分支");
+    }
+}
---
+++ TestGithubProject/src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -0,0 +1,10 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptTest
+ * discription:
+ * author:mx
+ * createTime:2018/11/29 9:38
+ */
+public class EmpController {
+}
